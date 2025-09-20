e6bf585e8afc6d51a16f5a4d842e76ac88775fa5
JosueRodLop <josrodlop19@alum.us.es>
2025-09-20 19:58:31 +0200

    fix: final version of project 4

--- src/main/java/com/example/VulnerableApp.java
+++ src/main/java/com/example/VulnerableApp.java
@@ -6,20 +6,19 @@ public class VulnerableApp {
 
     public static void processYaml(String yamlContent) {
         Yaml yaml = new Yaml();
-        // This start of payload is designed to guide the fuzzer towards the recursive structure
+        // This start of payload is designed to guide the fuzzer towards the recursive
+        // structure
         String yamlStr = "&a [ \"lol\"," + yamlContent;
         // This is the vulnerable part that can lead to a denial of service
         Object data = yaml.load(yamlStr);
-        // If we reach this point, the YAML was processed without exploiting the vulnerability
+        // If we reach this point, the YAML was processed without exploiting the
+        // vulnerability
         data.hashCode();
     }
+
     public static void main(String[] args) {
-        String payload = args[0];
-        try {
-            processYaml(payload);
-        } catch (Exception e) {
-            System.err.println("\n[!] Application crashed:");
-            e.printStackTrace();
-        }
+        // This main method should not be vulnerable as the input used in the vulnerable method is safe
+        String payload = "Hello, World!";
+        processYaml(payload);
     }
 }
